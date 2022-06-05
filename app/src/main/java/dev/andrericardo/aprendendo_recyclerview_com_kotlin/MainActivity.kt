ffe04c55a95dcacf0ee40a7b732fbbc02fcc0fe9
andrericardoweb <andrericardoweb@gmail.com>
2022-06-05 11:18:19 -0300

    first commit

---
+++ app/src/main/java/dev/andrericardo/aprendendo_recyclerview_com_kotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.andrericardo.aprendendo_recyclerview_com_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
